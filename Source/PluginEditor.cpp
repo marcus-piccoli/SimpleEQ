76a5eadd9a288accbcf4f1943a57157949686e39
marcus-piccoli <marcus.piccoli@gmail.com>
2025-05-04 19:26:03 -0300

    LowCutSlope Fixed

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -11,7 +11,14 @@
 
 //==============================================================================
 SimpleEqAudioProcessorEditor::SimpleEqAudioProcessorEditor (SimpleEqAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
+    : AudioProcessorEditor (&p), audioProcessor (p),
+peakFreqSliderAttachment(audioProcessor.apvts, "Peak Freq", peakFreqSlider),
+peakGainSliderAttachment(audioProcessor.apvts, "Peak Gain", peakGainSlider),
+peakQualitySliderAttachment(audioProcessor.apvts, "Peak Quality", peakQualitySlider),
+lowCutFreqSliderAttachment(audioProcessor.apvts, "LowCut Freq", lowCutFreqSlider),
+highCutFreqSliderAttachment(audioProcessor.apvts, "HighCut Freq", highCutFreqSlider),
+lowCutSlopeSliderAttachment(audioProcessor.apvts, "LowCut Slope", lowCutSlopeSlider),
+highCutSlopeSliderAttachment(audioProcessor.apvts, "HighCut Slope", highCutSlopeSlider)
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
@@ -46,8 +53,8 @@ void SimpleEqAudioProcessorEditor::resized()
     auto bounds = getLocalBounds();
     auto responseArea = bounds.removeFromTop(bounds.getHeight() * 0.33);
 
-    auto lowCutArea = bounds.removeFromLeft(bounds.getHeight() * 0.33);
-    auto highCutArea = bounds.removeFromRight(bounds.getHeight() * 0.33);
+    auto lowCutArea = bounds.removeFromLeft(bounds.getHeight() * 0.5);
+    auto highCutArea = bounds.removeFromRight(bounds.getHeight() * 0.5);
 
     lowCutFreqSlider.setBounds(lowCutArea.removeFromTop(lowCutArea.getHeight() * 0.5));
     lowCutSlopeSlider.setBounds(lowCutArea);
